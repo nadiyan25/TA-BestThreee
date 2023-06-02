57c19a5854e7b15a2f2a9a58e17a8d6ae49bd816
akbarpangestu20 <114888448+akbarpangestu20@users.noreply.github.com>
2023-06-02 18:00:57 +0700

    Upload resource/views/en_StokBarang.blade.php

---
+++ Front-End/resources/views/en_StokBarang.blade.php
@@ -0,0 +1,30 @@
+@extends('template_StokBarang')
+@section('title_template')
+    <title>Tambah Data StokBarang</title>
+@endsection
+
+@section('body_template')
+    <!-- section baris pertama -->
+    <section class="flex w-full">
+        <!-- section untuk Nama -->
+        <section class="bg-sky-500 w-1/2">
+            Nama
+        </section>
+        <!-- section untuk Harga -->
+        <section class="bg-rose-500 w-1/2">
+            Harga
+        </section>
+    </section>
+
+    <!-- section baris pertama -->
+    <section class="flex w-full">
+        <!-- section untuk Nama -->
+        <section class="bg-sky-500 w-1/2">
+            Nama
+        </section>
+        <!-- section untuk Harga -->
+        <section class="bg-rose-500 w-1/2">
+            Harga
+        </section>
+    </section>
+@endsection
