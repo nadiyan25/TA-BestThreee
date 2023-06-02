a7722568f5f84fc302ba8f2a5749acfbf8b51194
akbarpangestu20 <114888448+akbarpangestu20@users.noreply.github.com>
2023-06-02 18:02:16 +0700

    Upload resource/views/en_StokBarang.blade.php

---
+++ Front-End/resources/views/template_Stokbarang.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--csrf token -->
+    <meta name="_token" content="{{ csrf_token() }}">
+
+    @yield('title_karyawan')
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+<body class="m-5">
+    @yield('body_template')
+
+    <!-- CDN tail -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- cdn fontawesome (js) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
