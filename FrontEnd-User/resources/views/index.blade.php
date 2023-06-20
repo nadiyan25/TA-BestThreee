8eb44f9f516c2227192bee753d7e3b241e2ac191
deritjuniom <135311579+deritjuniom@users.noreply.github.com>
2023-06-20 16:07:38 +0700

    Update index.blade.php
    update

--- FrontEnd-User/resources/views/index.blade.php
+++ FrontEnd-User/resources/views/index.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Katalog Sparepart</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+    
 </body>
 </html>
