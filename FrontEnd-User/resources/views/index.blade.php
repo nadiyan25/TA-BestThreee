2499227567168864000b5ce1e37f373746558e86
akbarpangestu20 <114888448+akbarpangestu20@users.noreply.github.com>
2023-06-20 16:31:35 +0700

    Update index.blade.php
    Update

--- FrontEnd-User/resources/views/index.blade.php
+++ FrontEnd-User/resources/views/index.blade.php
@@ -243,6 +243,25 @@
             </div>
           </div>
 
+        <div class="col-lg-2 col-md-4 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="assets/img/fotooli/OLI-12.png" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <p>Oli</p>
+                <h4>Shell Helix</h4>
+                <div class="portfolio-links">
+                  <a href="assets/img/fotooli/OLI-12.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 3"><i class="bx bx-plus"></i></a>
+                  <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
 
               
               
