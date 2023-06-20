1a0c32959e707e7e16eb6758a8a70f1076df048e
akbarpangestu20 <114888448+akbarpangestu20@users.noreply.github.com>
2023-06-20 16:40:39 +0700

    Update index.blade.php
    Update

--- FrontEnd-User/resources/views/index.blade.php
+++ FrontEnd-User/resources/views/index.blade.php
@@ -382,6 +382,26 @@
 
       </div>
     </section><!-- End Contact Section -->
+
+     <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>BestThree Team</h3>
+      <p>Pemograman Web Berorientasi Service - IF GAB EXT</p>
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+      <div class="copyright">
+        &copy; Copyright <strong><span>BestThree</span></strong>. All Rights Reserved Universitas Teknokrat Indonesia
+      </div>
+      <div class="credits">
+      </div>
+    </div>
+  </footer><!-- End Footer -->
                 
               
               
