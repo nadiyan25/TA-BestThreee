d3c6f4d2cf75177c0bf4abcd410c82a8387020cb
akbarpangestu20 <114888448+akbarpangestu20@users.noreply.github.com>
2023-06-20 12:15:53 +0700

    Update template_Stokbarang.blade.php
    Update

--- Front-End/resources/views/template_Stokbarang.blade.php
+++ Front-End/resources/views/template_Stokbarang.blade.php
@@ -7,6 +7,12 @@
     <!--csrf token -->
     <meta name="_token" content="{{ csrf_token() }}">
 
+    <link
+      rel="shortcut icon"
+      href="/storage/website/favicon/favicon.png"
+      type="image/x-icon"
+    />
+
     @yield('title_karyawan')
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
